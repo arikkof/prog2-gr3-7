c734ba15ba7fec5d49d79f2522bc545967b9965f
amenta7 <mjakinnikova@hotmail.com>
2023-03-10 15:43:16 +0100

    Add Enum Genre and dummy movie list.

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,6 +1,7 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Movie {
@@ -8,9 +9,12 @@ public class Movie {
     private String description;
     // TODO add more properties here
 
-    public Movie(String title, String description) {
+    private List<Genre> genres;
+
+    public Movie(String title, String description, List<Genre> genres) {
         this.title = title;
         this.description = description;
+        this.genres = genres;
     }
 
     public String getTitle() {
@@ -23,6 +27,22 @@ public class Movie {
 
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie("Life Is Beautiful",
+                "When an open-minden Jewish librarian and his son become victims of the Holocaust, he uses a perfect mixture of will, humor and imagination to protect " +
+                        "his son from the dangers around their camp.",
+                Arrays.asList(Genre.DRAMA, Genre.ROMANCE)));
+        movies.add(new Movie("The Usual Suspects",
+                "A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which begin when five criminals meet at a seemingly random police lineup.",
+                Arrays.asList(Genre.CRIME, Genre.DRAMA, Genre.MYSTERY)));
+        movies.add(new Movie("Puss in Boots",
+                "An outlaw cat, his childhood egg-friend and seductive thief kitty set out in search for the eggs of the fabled Golden Goose to clear his name, restore his lost honor and regain the trust of his mother and town.",
+                Arrays.asList(Genre.COMEDY, Genre.FAMILY, Genre.ANIMATION)));
+        movies.add(new Movie("Avatar",
+                "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+                Arrays.asList(Genre.ANIMATION, Genre.DRAMA, Genre.ACTION)));
+        movies.add(new Movie("The Wolf of Wall Street",
+                "Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.",
+                Arrays.asList(Genre.DRAMA, Genre.ROMANCE, Genre.BIOGRAPHY)));
         // TODO add some dummy data here
 
         return movies;
